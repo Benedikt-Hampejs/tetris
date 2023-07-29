2006db0f99fe3e16f9d2992b8b58daf8a0601560
Benedikt Hampejs <benedikt.hampejs@icloud.com>
2023-07-29 14:00:49 +0200

    generate index.html by weback

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,34 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.ts',
   devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Shape Tracker',
+      template: './src/index.html',
+      inject: 'body'
+    })
+  ],
   module: {
     rules: [
       {
+        test: /\.ts$/, 
         use: 'ts-loader',
         exclude: /node_modules/,
       },
     ],
   },
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
+  },
+  devServer: {
+    static: './dist',
   },
 };
